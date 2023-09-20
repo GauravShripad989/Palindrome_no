c5eaf60e1123fb046d21bd8b3eb7b7457386c5a9
GauravShripad <gauravs.csea19@sbjit.edu.in>
2023-09-20 09:23:56 +0530

    Palindrome number

--- src/Palindrome_no.java
+++ src/Palindrome_no.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Palindrome_no {
+public class Palindrome_no {
+    public static void main(String[] args){
+        int r, sum=0,temp;
+        int n = 565;
+
+        temp = n;
+        while(n>0){
+            r = n%10;
+            sum = (sum*10)+r;
+            n=n/10;
+        }
+        if(temp==sum){
+            System.out.println("Palindrome No." + sum);
+        }else{
+            System.out.println("not a palindrome no."+ sum);
+        }
+    }
 }
